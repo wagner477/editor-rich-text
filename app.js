4f23dba836c94c503f6c91fa93cdeaa7c434f394
Wagner Oliveira de Almeida <61123555+drk477@users.noreply.github.com>
2021-10-17 00:29:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ editor.document.designMode = "On";
 
 function transform(option, argument, id) {
   editor.document.execCommand(option, false, argument);
-  console.log(editor);
   const element = document.getElementById(id);
   if (element.classList.length >= 1) {
     element.classList.remove("bg-purple");
